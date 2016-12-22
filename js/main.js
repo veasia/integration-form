8e9f597a54c220929c0ccfe7febaf9890c4f6344
mrvoid <enis.nefzi.sanchez@gmail.com>
2016-12-22 11:11:33 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var endpoint = 'https://api.veinteractive.com/IntegrationsApiSandbox/Install';
+var key = '410babbf5fec48e1b683f42d0d938b4e';	
+
+function submit() {
+	var payload = {
+		Domain: document.getElementById('domain'),
+		Email:document.getElementById('email'),
+	 	Country:document.getElementById('country'),
+	 	Currency:document.getElementById('currency'),
+	 	Language:document.getElementById('language'),
+	 	Phone:document.getElementById('phone'),
+		ECommerce:document.getElementById('agency'),
+		ECommercePlatform:document.getElementById('eccomerce'),
+		Version:document.getElementById('version'),
+		Merchant:document.getElementById('merchant'),
+	};
+
+   	var xhr = new XMLHttpRequest();
+    xhr.open("POST", endpoint);
+    xhr.setRequestHeader("Ocp-Apim-Subscription-Key", key);
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.send(payload);
+
+	if(xhr.statusCode != 200) {
+		window.location = "./error.html";
+	} else {
+		//Save request output to fill the success fields
+		window.location = "./complete.html";
+	}
+}
